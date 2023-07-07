98453c3acd3c5a3f7cbe41f98c5e82a4ab099778
Kamil Kulka <kamil.kulka@badgermapping.com>
2023-07-07 11:44:23 +0200

    Initial Commit

---
+++ ShoppingCart/ShoppingCartApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingCartApp.swift
+//  ShoppingCart
+//
+//  Created by Kamil Kulka on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingCartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
