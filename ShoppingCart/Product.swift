0f63dd89cd0adab913a9d0572d1f326fbd2ab9c4
Serhii Kostanian <sergii.kostanian@gmail.com>
2023-07-07 12:57:01 +0200

    Add the Product model

---
+++ ShoppingCart/Product.swift
@@ -0,0 +1,16 @@
+//
+//  Product.swift
+//  ShoppingCart
+//
+//  Created by Serhii Kostanian on 07.07.2023.
+//
+
+import Foundation
+
+struct Product {
+    let name: String
+    let icon: String
+    let cost: Double
+    let revenue: Double
+    let tax: Double
+}
