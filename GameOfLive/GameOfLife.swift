425100b870d020a5765b41a5bf55d0d38131d4da
Serhii Kostanian <sergii.kostanian@gmail.com>
2023-08-04 13:53:44 +0200

    Draft implementation of the game of life

--- GameOfLive/GameOfLife.swift
+++ GameOfLive/GameOfLife.swift
@@ -3,10 +3,65 @@ import Foundation
 typealias GameOfLife = Array<Array<String>>
 
 extension GameOfLife {
+
     func nextGeneration() -> GameOfLife {
-         return [
-        ["☠️","☠️","☠️"],
-        ["☠️","☠️","☠️"],
-        ["☠️","☠️","☠️"]]
+        var nextGeneration = self
+
+        for row in 0..<self.count {
+            for col in 0..<self[row].count {
+                var aliveNeighboursCount = 0
+
+                // --- First row
+                if isNeighbourAlive(i: row - 1, j: col - 1) {
+                    aliveNeighboursCount += 1
+                }
+                if isNeighbourAlive(i: row, j: col - 1) {
+                    aliveNeighboursCount += 1
+                }
+                if isNeighbourAlive(i: row + 1, j: col - 1) {
+                    aliveNeighboursCount += 1
+                }
+
+                // --- Second row
+                if isNeighbourAlive(i: row - 1, j: col) {
+                    aliveNeighboursCount += 1
+                }
+                if isNeighbourAlive(i: row + 1, j: col) {
+                    aliveNeighboursCount += 1
+                }
+
+                // --- Third row
+                if isNeighbourAlive(i: row - 1, j: col + 1) {
+                    aliveNeighboursCount += 1
+                }
+                if isNeighbourAlive(i: row, j: col + 1) {
+                    aliveNeighboursCount += 1
+                }
+                if isNeighbourAlive(i: row + 1, j: col + 1) {
+                    aliveNeighboursCount += 1
+                }
+
+                let isCurrentCellAlive = self[row][col] == "❤️"
+
+                switch (isCurrentCellAlive, aliveNeighboursCount) {
+                case (true, 2), (true, 3): // #3
+                    nextGeneration[row][col] = "❤️"
+                case (false, 3): // #4
+                    nextGeneration[row][col] = "❤️"
+                default:
+                    nextGeneration[row][col] = "☠️"
+                }
+            }
+        }
+
+        return nextGeneration
+    }
+
+    private func isNeighbourAlive(i: Int, j: Int) -> Bool {
+        return i >= 0 &&
+            j >= 0 &&
+            i < self.count &&
+            j < self[0].count &&
+           self[i][j] == "❤️"
     }
 }
--- ShoppingCartTests/GameOfLifeTests.swift
+++ ShoppingCartTests/GameOfLifeTests.swift
@@ -2,16 +2,69 @@ import Foundation
 import XCTest
 @testable import ShoppingCart
 
-func testGIVEN_cell_with_one_neighbour_is_at_grid_index1_1_WHEN_whowing_next_generation_THEN_cell_should_be_dead(){
-    let grid = [
-    ["☠️","☠️","☠️"],
-    ["☠️","❤️","☠️"],
-    ["☠️","☠️","☠️"]]
-    
-    //WHEN
-    let nextGenerationGrid = grid.nextGeneration()
-    
-    //THEN
-    XCTAssertEqual(nextGenerationGrid[1][1],"☠️")
-}
+final class GameOfLifeTests: XCTestCase {
 
+    // 1. Any live cell with fewer than two live neighbours dies, as if caused by underpopulation
+    func testFirstRule() {
+        // GIVEN only center cell alive
+        let grid = [
+            ["☠️","☠️","☠️"],
+            ["☠️","❤️","☠️"],
+            ["☠️","☠️","☠️"]]
+
+        // WHEN showing next generation
+        let nextGenerationGrid = grid.nextGeneration()
+
+        // THEN center cell should be dead
+        XCTAssertEqual(nextGenerationGrid[1][1],"☠️")
+    }
+
+    // 2. Any live cell with more than three live neighbours dies, as if by overcrowding
+    func testSecondRule() {
+        let grid = [
+            ["❤️","❤️","❤️"],
+            ["❤️","❤️","☠️"],
+            ["☠️","☠️","☠️"]
+        ]
+
+        //WHEN
+        let nextGenerationGrid = grid.nextGeneration()
+
+        //THEN
+        let expectedGrid = [
+            ["❤️","☠️","❤️"],
+            ["❤️","☠️","❤️"],
+            ["☠️","☠️","☠️"]
+        ]
+        XCTAssertEqual(nextGenerationGrid, expectedGrid)
+    }
+
+    func testBlinker() {
+        let grid = [
+            ["☠️","☠️","☠️","☠️","☠️"],
+            ["☠️","☠️","☠️","☠️","☠️"],
+            ["☠️","❤️","❤️","❤️","☠️"],
+            ["☠️","☠️","☠️","☠️","☠️"],
+            ["☠️","☠️","☠️","☠️","☠️"]
+        ]
+
+        //WHEN
+        var nextGenerationGrid = grid.nextGeneration()
+
+        //THEN
+        let expectedGrid = [
+            ["☠️","☠️","☠️","☠️","☠️"],
+            ["☠️","☠️","❤️","☠️","☠️"],
+            ["☠️","☠️","❤️","☠️","☠️"],
+            ["☠️","☠️","❤️","☠️","☠️"],
+            ["☠️","☠️","☠️","☠️","☠️"]
+        ]
+        XCTAssertEqual(nextGenerationGrid, expectedGrid)
+
+        //WHEN
+        nextGenerationGrid = nextGenerationGrid.nextGeneration()
+
+        //THEN
+        XCTAssertEqual(grid, nextGenerationGrid)
+    }
+}
